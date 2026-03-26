cc70fb9a49ae97c8f1196eaec4b3c9651a5b260e
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-03-26 02:25:07 -0300

    Update lay_2_ypo.py

--- lay_2_ypo.py
+++ lay_2_ypo.py
@@ -429,7 +429,7 @@ def abre(nome_do_tema):
     return lista
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data()
 def load_babel():
     lista = []
     with open(os.path.join("./base/babel.txt"), "r") as babel:
