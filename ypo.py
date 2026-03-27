a6cd25a7cabddc834b38baed81c990eb751794f9
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-03-27 13:58:26 -0300

    Update ypo.py

--- ypo.py
+++ ypo.py
@@ -620,7 +620,7 @@ def load_poema(nome_tema, seed_eureka):  # generate new yPoema
 #    frufru = "* " + nome_tema + " *" + "<br>"
 #    novo_ypoema += f"<h3 style='text-align: center;'>* {frufru} *</h3>\n\n" + unsafe_allow_html=True
 
-    frufru = f"<div style='text-align: center; font-size: 24px; margin-bottom: 20px;'><strong>* {nome_tema} *</strong></div>"
+    frufru = f"<div style='text-align: center; font-size: 32px; margin-bottom: 20px;'><strong>* {nome_tema} *</strong></div>"
     st.markdown(frufru, unsafe_allow_html=True)    
 
     with open(os.path.join("./temp/" + lypo_user), "w", encoding="utf-8") as save_lypo:
