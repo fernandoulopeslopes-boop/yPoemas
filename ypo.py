72c4739f5c4a8274f7c4c0d00e5c578bc00445f8
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-03-29 16:45:35 -0300

    Update ypo.py

--- ypo.py
+++ ypo.py
@@ -934,7 +934,8 @@ def page_ypoemas():
                 texto_formatado = "  \n".join(linhas_formatadas)
             else:
                 texto_formatado = "Gerando versos..."
-            # --- ATÉ AQUI ---        with st.expander(what_book, expanded=True):
+            # --- ATÉ AQUI ---       
+       with st.expander(what_book, expanded=True):
         
             # 1. MOTOR DE TEXTO (Geração e Tradução)
             if st.session_state.lang != st.session_state.last_lang:
