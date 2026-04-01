45b0bf424728feaddc1e5c17e1d2cde8450af7f2
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-04-01 01:45:35 -0300

    Update ypo.py

--- ypo.py
+++ ypo.py
@@ -109,8 +109,8 @@ def page_ypoemas():
         st.info(f"Matriz: {st.session_state.tema}")
 
     with c_obra:
-        # CORREÇÃO: Passando o tema e a semente (take) para o motor
-        poema = gera_poema(st.session_state.tema, st.session_state.take)
+        # CORREÇÃO: Convertendo take para string para evitar erro no .partition() do motor
+        poema = gera_poema(st.session_state.tema, str(st.session_state.take))
         url_teste = "https://images.unsplash.com/photo-1454117096348-e4abbeae002c?w=500"
         write_ypoema(st.session_state.tema, poema, URL_IMAGEM=url_teste)
 
