442490abd06036ba961ac7f5236ffef02bfeca44
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-04-25 00:39:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ if have_internet():
     try:
         from gtts import gTTS
     except ImportError as ex:
-        pass.
-        #st.write("Google TTS não conectado")
+        st.write("Google TTS não conectado")
 else:
     st.warning("Internet não conectada. Traduções não disponíveis no momento.")
 
