bf849a19cdb72e0b1363f964822d2880fd2e1345
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-04-28 15:55:53 -0300

    Update ypo_seguro.py

--- ypo_seguro.py
+++ ypo_seguro.py
@@ -11,7 +11,7 @@ import random
 import base64
 import socket
 import streamlit as st
-from lay_2_ypo import gera_poema()
+from lay_2_ypo import gera_poema
 
 from extra_streamlit_components import TabBar as stx
 from datetime import datetime
