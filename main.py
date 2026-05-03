3f3dac76c4b795340dc518030472b263bcce8607
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-03 04:12:18 -0300

    Change default book session state value

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ if "last_lang" not in st.session_state:
     st.session_state.last_lang = "pt"
 
 if "book" not in st.session_state:  #  index for books_list
-    st.session_state.book = "todos os livros"
+    st.session_state.book = "todos os temas"
 if "take" not in st.session_state:  #  index for selected tema in books_list
     st.session_state.take = 0
 if "mini" not in st.session_state:  #  index for selected tema in page_mini
