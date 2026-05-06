142394ccac95f7153d11255a9e94c42761d3d005
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-06 04:23:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1490,7 +1490,7 @@ def main():
         ##$ page_docs()
 
     with st.sidebar:
-        st.image(lower("/images/" + magy))
+        st.image("/images/" + magy.lower)
 
     show_icons()
     ##$ st.sidebar.state = True
