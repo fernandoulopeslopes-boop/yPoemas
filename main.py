9194ce8a0d0289c01a430b0782c0ea1797a5a963
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-09 08:51:25 -0300

    Improve state management and add temas selection
    Refactor session state handling and add selectbox for temas.

--- main.py
+++ main.py
@@ -845,32 +845,39 @@ def page_ypoemas():
     nest = nest.button("▶", help=help_nest)
     manu = manu.button("?", help="help !!!")
 
-    if more:
-        st.rerun()
-        
     if last:
         st.session_state.take -= 1
         if st.session_state.take < 0:
             st.session_state.take = maxy_ypoemas
-            st.rerun()
 
     if rand:
         st.session_state.take = random.randrange(0, maxy_ypoemas)
-        st.rerun()
 
     if nest:
         st.session_state.take += 1
         if st.session_state.take > maxy_ypoemas:
             st.session_state.take = 0
-            st.rerun()
+
+    if not st.session_state.draw:
+        options = list(range(len(temas_list)))
+        sobrios = "↓  " + translate("lista de Temas")
+        opt_take = st.selectbox(
+            sobrios,
+            options,
+            index=st.session_state.take,
+            format_func=lambda z: temas_list[z],
+            key="opt_take",
+        )
+
+        if opt_take != st.session_state.take:
+            st.session_state.take = opt_take
 
     st.session_state.tema = temas_list[st.session_state.take]
-    st.rerun()
 
+    lnew = True
     if manu:
         st.subheader(load_md_file("MANUAL_YPOEMAS.md"))
 
-    lnew = True
     if lnew:
         what_book = (
             "⚫  "
@@ -924,7 +931,6 @@ def page_ypoemas():
             talk(curr_ypoema)
 
         # st.markdown(get_binary_file_downloader_html('./temp/'+'LYPO_' + IPAddres, '➪ '+st.session_state.tema), unsafe_allow_html=True)
-
 
 def page_eureka():
     help_tips = load_help(st.session_state.lang)
