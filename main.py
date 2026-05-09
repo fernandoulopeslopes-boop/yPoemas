9eff8a7fac5677701b97c08e1ba2557427bbcc70
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-09 05:18:51 -0300

    Fix image variable assignment and path
    Updated image path handling for sidebar images.

--- main.py
+++ main.py
@@ -1461,36 +1461,36 @@ def main():
 
     if chosen_id == "1":
         st.sidebar.info(load_md_file("INFO_MINI.md"))
-        magy = "img_mini.jpg"
+         = "img_mini.jpg"
         page_mini()
     elif chosen_id == "2":
         st.sidebar.info(load_md_file("INFO_YPOEMAS.md"))
-        magy = "img_ypoemas.jpg"
+         = "img_ypoemas.jpg"
         page_ypoemas()
     elif chosen_id == "3":
         st.sidebar.info(load_md_file("INFO_EUREKA.md"))
-        magy = "img_eureka.jpg"
+         = "img_eureka.jpg"
         page_eureka()
     elif chosen_id == "4":
         st.sidebar.info(load_md_file("INFO_OFF-MACHINA.md"))
-        magy = "img_off-machina.jpg"
+         = "img_off-machina.jpg"
         page_off_machina()
     elif chosen_id == "5":
         st.sidebar.info(load_md_file("INFO_BOOKS.md"))
-        magy = "img_books.jpg"
+         = "img_books.jpg"
         page_books()
     elif chosen_id == "6":
         st.sidebar.info(load_md_file("INFO_POLY.md"))
-        magy = "img_poly.jpg"
+         = "img_poly.jpg"
         page_polys()
     elif chosen_id == "7":
         st.sidebar.info(load_md_file("INFO_ABOUT.md"))
-        magy = "img_about.jpg"
+         = "img_about.jpg"
         page_abouts()
         ##$ page_docs()
 
     with st.sidebar:
-        st.image(magy)
+        st.image("\images\" + magy)
 
     show_icons()
     ##$ st.sidebar.state = True
