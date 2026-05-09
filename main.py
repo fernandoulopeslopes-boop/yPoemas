dedb5e0910d66f24ca8a463fb05f02691f0de295
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-09 07:47:11 -0300

    Fix image path in sidebar to use relative path

--- main.py
+++ main.py
@@ -1516,7 +1516,7 @@ def main():
         ##$ page_docs()
 
     with st.sidebar:
-        st.image("\images\" + magy)
+        st.image("./images/" + magy)
 
     show_icons()
     ##$ st.sidebar.state = True
