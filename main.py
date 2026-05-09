2333df16a04c89a8af8491e0f5c9f2b75504c312
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-09 10:14:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1322,7 +1322,7 @@ def page_polys():  # available languages
         st.session_state.last_lang = st.session_state.lang
         st.session_state.lang = st.session_state.poly_lang
 
-    lNew = .t.
+    lNew = True
     if lnew:
         poly_expander = st.expander("", True)
         with poly_expander:
