33b8a9c54e31d534a9ac5bf81976f52e4a11186f
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-09 05:26:51 -0300

    Fix variable assignment for image paths

--- main.py
+++ main.py
@@ -1465,27 +1465,27 @@ def main():
         page_mini()
     elif chosen_id == "2":
         st.sidebar.info(load_md_file("INFO_YPOEMAS.md"))
-         = "img_ypoemas.jpg"
+        magy = "img_ypoemas.jpg"
         page_ypoemas()
     elif chosen_id == "3":
         st.sidebar.info(load_md_file("INFO_EUREKA.md"))
-         = "img_eureka.jpg"
+        magy = = "img_eureka.jpg"
         page_eureka()
     elif chosen_id == "4":
         st.sidebar.info(load_md_file("INFO_OFF-MACHINA.md"))
-         = "img_off-machina.jpg"
+        magy = "img_off-machina.jpg"
         page_off_machina()
     elif chosen_id == "5":
         st.sidebar.info(load_md_file("INFO_BOOKS.md"))
-         = "img_books.jpg"
+        magy = "img_books.jpg"
         page_books()
     elif chosen_id == "6":
         st.sidebar.info(load_md_file("INFO_POLY.md"))
-         = "img_poly.jpg"
+         magy = "img_poly.jpg"
         page_polys()
     elif chosen_id == "7":
         st.sidebar.info(load_md_file("INFO_ABOUT.md"))
-         = "img_about.jpg"
+        magy = "img_about.jpg"
         page_abouts()
         ##$ page_docs()
 
