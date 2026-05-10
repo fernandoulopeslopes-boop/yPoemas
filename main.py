e2743b02fa99d29867bafa4cf97d07d6a235b43d
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-10 06:21:41 -0300

    Rename selectbox label for language selection

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
         idiomas_base = ["Português", "English", "Español", "Français", "Deutsch", "Italiano"]
         extensao = sorted(["Català", "Dansk", "Euskara", "Suomi", "Galego", "Islandska", "Lëtzebuergesch", "Magyar", "Nederlands", "Norsk", "Polski", "Portuñol", "Română", "Slovenčina", "Slovenščina"]) + ["Russia", "Suécia"]
         
-        st.session_state.idioma = st.selectbox("Translator", idiomas_base + extensao)
+        st.session_state.idioma = st.selectbox("Idiomas disponíveis...", idiomas_base + extensao)
         
         st.divider()
 
