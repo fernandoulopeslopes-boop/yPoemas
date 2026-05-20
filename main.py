ecc8dfca4531aa620d1008823e6c62dac66909fc
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-19 23:17:14 -0300

    Refactor language file definitions and mappings
    Removed unused language files and updated language mappings.

--- main.py
+++ main.py
@@ -49,38 +49,33 @@ PAGE_INFO_FILES = {
     "4": "INFO_OFF-MACHINA.md", "5": "INFO_BOOKS.md", "6": "INFO_POLY.md", "7": "INFO_ABOUT.md",
 }
 
-LANG_FILES = {
-    "pt": "poly_pt.txt", "es": "poly_es.txt", "it": "poly_it.txt",
-    "fr": "poly_fr.txt", "en": "poly_en.txt",
-}
-
 VOICES_EDGE_TTS = {
     "pt": "pt-BR-AntonioNeural", "en": "en-US-GuyNeural", "es": "es-ES-AlvaroNeural",
     "fr": "fr-FR-RemyNeural", "it": "it-IT-DiegoNeural",
 }
 
 IDIOMAS_OFICIAIS = [
-    ("Português", "Brasil", "pt", "poly_pt.txt"),
-    ("English", "Inglaterra", "en", "poly_en.txt"),
-    ("Español", "Espanha", "es", "poly_es.txt"),
-    ("Français", "França", "fr", "poly_fr.txt"),
-    ("Italiano", "Itália", "it", "poly_it.txt"),
-    ("Deutsch", "Alemanha", "de", "poly_de.txt"),
-    ("Català", "Catalunha", "ca", "poly_ca.txt"),
-    ("Galego", "Galícia", "gl", "poly_gl.txt"),
-    ("Nederlands", "Países Baixos", "nl", "poly_nl.txt"),
-    ("Polski", "Polônia", "pl", "poly_pl.txt"),
-    ("Română", "Romênia", "ro", "poly_ro.txt"),
-    ("Русский", "Rússia", "ru", "poly_ru.txt"),
-    ("Svenska", "Suécia", "sv", "poly_sv.txt"),
-    ("Norsk", "Noruega", "no", "poly_no.txt"),
-    ("Dansk", "Dinamarca", "da", "poly_da.txt"),
-    ("Suomi", "Finlândia", "fi", "poly_fi.txt"),
-    ("Íslenska", "Islândia", "is", "poly_is.txt"),
-    ("Magyar", "Hungria", "hu", "poly_hu.txt"),
-    ("Latin", "Latim", "la", "poly_la.txt"),
-    ("Esperanto", "Esperanto", "eo", "poly_eo.txt"),
-    ("Portuñol", "Portunhol", "pt", "poly_pt.txt"),
+    'Português': 'pt',
+    'Espanhol': 'es',
+    'Italiano': 'it',
+    'Francês': 'fr',
+    'Inglês': 'en',
+    'Esperanto': 'eo',
+    'Latin': 'la',
+    'Basco': 'eu',
+    'Catalão': 'ca',
+    'Córsico': 'co',
+    'Galego': 'gl',
+    'Galês': 'cy',
+    'Polonês': 'pl',
+    'Holandês': 'nl',
+    'Irlandês': 'ga',
+    'Norueguês': 'no',
+    'Finlandês': 'fi',
+    'Dinamarquês': 'da',
+    'Romeno': 'ro',
+    'Russo': 'ru'
+    'Sueco': 'sv',
 ]
 
 FONTES_MACHINA = [
