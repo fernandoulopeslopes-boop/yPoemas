9e5626ca9cd371351c708d13c1a48b419aedb4e1
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-28 20:44:58 -0300

    Update ypo_seguro.py

--- ypo_seguro.py
+++ ypo_seguro.py
@@ -1436,7 +1436,7 @@ def page_ypoemas():
     ):  # just in case
         st.session_state.take = 0
 
-    col_livros, col_nav, col_temas = st.columns([3.6, 2.8, 3.6])
+    col_livros, col_nav, col_temas = st.columns([3, 4, 3.)
 
     with col_livros:
         pick_book_sidebar("palco")
