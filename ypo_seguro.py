96b448f8f45c8a53eafdd7effdb23cbb30212bc3
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-05-29 06:24:55 -0300

    Update ypo_seguro.py

--- ypo_seguro.py
+++ ypo_seguro.py
@@ -476,7 +476,7 @@ def init_session_state():
         "sidebar_panel": "Machina",
         "cia_name": "",
         "cia_mood": "Sintática",
-        "cia_line0_offset_px": -365,
+        "cia_line0_offset_px": -370,
 
         # chave de ouro
         "key_open": False,
