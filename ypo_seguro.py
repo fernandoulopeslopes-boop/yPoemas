d8b31217075aec473443a435f5662c38215b932b
fernandoulopeslopes-boop <fernandoulopeslopes@gmail.com>
2026-06-02 20:39:20 -0300

    Update ypo_seguro.py

--- ypo_seguro.py
+++ ypo_seguro.py
@@ -2031,7 +2031,7 @@ def main():
                     stx.TabBarItemData(id=1, title="mini", description=""),
                     stx.TabBarItemData(id=2, title="yPoemas", description=""),
                     stx.TabBarItemData(id=3, title="eureka", description=""),
-                    stx.TabBarItemData(id=4, title="off-mach", description=""),
+                    stx.TabBarItemData(id=4, title="off-Machina", description=""),
                     stx.TabBarItemData(id=5, title="about", description=""),
                 ],
                 default=2,
