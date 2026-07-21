c6b5b0d6ca459bba47b0ea528c1d9e99674afe6a
Fernando Lopes <fernandoulopeslopes@gmail.com>
2026-07-21 08:34:33 -0300

    Update mobyle.py

--- mobyle.py
+++ mobyle.py
@@ -1,5 +1,4 @@
 import os
-import os
 import re
 import time
 import random
@@ -2144,7 +2143,7 @@ def render_copy_bundle_widget(texto, token, qtd_real=None):
 @st.cache_data
 def load_images():
     images_list = []
-    with open(os.path.join("./base/images.txt"), encoding="utf-8") as lista:
+    with open(os.path.join("./base/ativos.txt"), encoding="utf-8") as lista:
         for line in lista:
             images_list.append(line)
 
