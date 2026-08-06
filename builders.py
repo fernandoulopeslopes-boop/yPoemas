ee2c7baa492c41757f01d54276b0b1d2c92a42d4
Fernando Lopes <fernandoulopeslopes@gmail.com>
2026-08-06 11:27:43 -0300

    Create builders.py for build functions import

---
+++ builders.py
@@ -0,0 +1,5 @@
+"""Compatibilidade mínima: a fonte única da família Builds é Tools.py."""
+
+from tools import build_utf8_temas, build_lexico, build_indexy
+
+__all__ = ["build_utf8_temas", "build_lexico", "build_indexy"]
